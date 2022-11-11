9d67042d6dcfa6e54f8403783fe27066c9205239
ahmedhagag900 <ahmedhegag900@gmail.com>
2022-11-11 23:14:01 +0200

    Add project files.

---
+++ src/FlapKap.API/Program.cs
@@ -0,0 +1,36 @@
+using FlapKap.API.Configuration;
+
+namespace FlapKap.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder = builder.ConfigureBaseServices();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        
+
+    }
+}
---
+++ src/FlapKap.API/ServiceConfiguration/ServiceConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace FlapKap.API.Configuration
+{
+    public static class ServiceConfiguration
+    {
+        public static WebApplicationBuilder ConfigureBaseServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            return builder;
+        }
+    }
+}
